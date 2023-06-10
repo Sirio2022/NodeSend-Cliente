937cf3342ec07ca44cd62ca0a786acdf55c0c5e6
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-06-10 14:24:14 -0500

    Melo
    Con el nuevo Routing

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <header className="py-8 flex flex-col md:flex-row justify-between items-center">
-      <Image
-        src="logo.svg"
-        alt="logo"
-        width={150}
-        height={33}
-        className="w-64 mb-8 md:b-0"
-        priority
-      />
+      <Link href="/" rel='preLoad' as="/">
+        <Image
+          src="logo.svg"
+          alt="logo"
+          width={150}
+          height={33}
+          className="w-64 mb-8 md:b-0"
+          priority
+        />
+      </Link>
+
+      <div>
+        <Link
+          href="/login"
+          className="bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2"
+          rel="preload"
+          as="login"
+        >
+          Iniciar Sesión
+        </Link>
+        <Link
+          href="/crearcuenta"
+          className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase"
+          rel="preload"
+          as="crearcuenta"
+        >
+          Crear Cuenta
+        </Link>
+      </div>
     </header>
   );
 }
---
+++ src/app/crearcuenta/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default function CrearCuanta() {
+  return (
+    <Layout>
+      <h1>Crear Cuenta</h1>
+    </Layout>
+  );
+}
