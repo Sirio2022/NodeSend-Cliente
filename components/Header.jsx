a1d31871488db4f97c9641d21e69837b89fd6017
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-06-18 18:00:34 -0500

    Brutal

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,37 @@
 import Image from 'next/image';
 import React from 'react';
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="py-8 flex flex-col md:flex-row items-center justify-between">
-      <Image src="/logo.svg" width={142} height={142} alt="Logo" className='w-64 mb-8 md:mb-0' />
-    </header>
+    <>
+      <header className="py-8 flex flex-col md:flex-row items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/logo.svg"
+            width={142}
+            height={142}
+            alt="Logo"
+            className="w-64 mb-8 md:mb-0"
+            priority={true}
+          />
+        </Link>
+
+        <div>
+          <Link
+            className="bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2"
+            href="/login"
+          >
+            Iniciar sesión
+          </Link>
+          <Link
+            href="/crearcuenta"
+            className="bg-black px-5 py-3 rounded-lg text-white font-bold uppercase"
+          >
+            Crear cuenta
+          </Link>
+        </div>
+      </header>
+    </>
   );
 }
--- pages/crearcuenta.jsx
+++ pages/crearcuenta.jsx
@@ -8,15 +8,20 @@ export default function crearcuenta() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const formik = useFormik({
     initialValues: {
+      nombre: '',
       email: '',
       password: '',
     },
     validationSchema: Yup.object({
+      nombre: Yup.string().required('El nombre es obligatorio'),
+
       email: Yup.string()
         .email('El Email no es válido')
         .required('El Email es obligatorio'),
 
-      password: Yup.string().required('El password es obligatorio'),
+      password: Yup.string()
+        .required('El password es obligatorio')
+        .min(6, 'El password debe ser de al menos 6 caracteres'),
     }),
     onSubmit: (values) => {
       console.log(values);
@@ -27,7 +32,7 @@ export default function crearcuenta() {
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
         <h2 className="text-4xl text-center font-bold text-gray-800 my-4 font-sans">
-          Iniciar Sesión
+          Crear Cuenta
         </h2>
 
         <div className="flex justify-center mt-5">
@@ -36,6 +41,29 @@ export default function crearcuenta() {
               className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
               onSubmit={formik.handleSubmit}
             >
+              <div className="mb-4">
+                <label
+                  className="block text-black text-sm font-bold mb-2"
+                  htmlFor="nombre"
+                >
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="nombre"
+                  placeholder="Nombre de usuario"
+                  value={formik.values.nombre}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.touched.nombre && formik.errors.nombre ? (
+                  <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                    <p className="font-bold">Error</p>
+                    <p>{formik.errors.nombre}</p>
+                  </div>
+                ) : null}
+              </div>
               <div className="mb-4">
                 <label
                   className="block text-black text-sm font-bold mb-2"
@@ -85,7 +113,7 @@ export default function crearcuenta() {
               <input
                 type="submit"
                 className="bg-red-500 hover:bg-gray-900 w-full p-2 text-white uppercase font-bold cursor-pointer"
-                value="Iniciar Sesión"
+                value="Crear cuenta"
               />
             </form>
           </div>
